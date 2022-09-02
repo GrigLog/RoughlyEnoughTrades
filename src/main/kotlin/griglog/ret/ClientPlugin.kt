a1d1504b7490d55040c56d8f313d3c4f03aef67b
GrigLog <griglogvin@gmail.com>
2022-09-02 10:16:13 +0300

    Category workstation (villager egg)

--- src/main/kotlin/griglog/ret/ClientPlugin.kt
+++ src/main/kotlin/griglog/ret/ClientPlugin.kt
@@ -5,15 +5,18 @@ import griglog.ret.villager_trades.villagerTradesRegister
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry
-import net.minecraft.resources.ResourceLocation
+import me.shedaniel.rei.api.common.util.EntryIngredients
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 
 
 class ClientPlugin : REIClientPlugin{
-    override fun registerCategories(registry: CategoryRegistry?) {
-        registry?.add(TradesCategory())
+    override fun registerCategories(registry: CategoryRegistry) {
+        registry.add(TradesCategory())
+        registry.addWorkstations(TradesCategory.category, EntryIngredients.of(ItemStack(Items.VILLAGER_SPAWN_EGG)))
     }
 
-    override fun registerDisplays(registry: DisplayRegistry?) {
-        villagerTradesRegister(registry!!)
+    override fun registerDisplays(registry: DisplayRegistry) {
+        villagerTradesRegister(registry)
     }
 }
